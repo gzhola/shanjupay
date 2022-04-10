22ac078496bd05f3d211a4223f2db0d77016439b
Jiang <cutuopen@aliyun.com>
2022-04-10 21:59:38 +0800

    [新增]初始化API项目架构

---
+++ shanjupay/shanjupay-merchant-application/src/main/java/com/shanjupay/merchant/MerchantApplication.java
@@ -0,0 +1,17 @@
+package com.shanjupay.merchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jiang
+ * @since  2022-04-10
+ */
+@SpringBootApplication
+public class MerchantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchantApplication.class, args);
+    }
+
+}
